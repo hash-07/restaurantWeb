a4e5fef08bcb994cc8e00399421305e6f2c2d156
hash <hasheer11@gmail.com>
2021-04-25 14:01:02 +0530

    "real"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Holiday from './components/Holiday';
+import HolidaySearch1 from './components/HolidaySearch1';
+import Header from './components/Header';
+import HolidayS1Card from './cards/HolidayS1Card';
+import HolidayS2Card from './cards/HolidayS2Card';
+import Holiday2 from './components/Holiday2';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello hash</h1>
-      <h1>hello hash</h1>
-      <h1>hello hash</h1>
+      {/* <Header/> */}
+      {/* <Holiday/> */}
+      {/* <Holiday2/> */}
+      <HolidaySearch1/>
+      {/* <HolidayS1Card/> */}
+      {/* <HolidayS2Card/> */}
+      
     </div>
   );
 }
